663e78abebdcaebbf5226c38013c100f6b37b5ec
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2023-01-02 15:30:28 -0500

    Update sounddist.py

--- odas/sounddist.py
+++ odas/sounddist.py
@@ -22,5 +22,21 @@ def closest_microphone(x):
 	distance_mic4 = z[3] * fs * 0.5 * d
 	return min(distance_mic1, distance_mic2, distance_mic3, distance_mic4)
 
-print(closest_microphone(x))
+def direction_of_arrival(x):
+	# Set the sampling frequency (in Hz)
+	fs = 44100
 
+	# Set the distance between microphones (in meters)
+	d = 0.035
+
+	# Estimate the distance of the sound source from each microphone using the GCC algorithm
+	z = odas.gcc(x, fs)
+	distance_mic1 = z[0] * fs * 0.5 * d
+	distance_mic2 = z[1] * fs * 0.5 * d
+	distance_mic3 = z[2] * fs * 0.5 * d
+	distance_mic4 = z[3] * fs * 0.5 * d
+	angle = math.atan2(distance_mic3 - distance_mic1, distance_mic2 - distance_mic4) * 180 / math.print
+	return angle
+
+print(closest_microphone(x))
+print(direction_of_arrival(x))
