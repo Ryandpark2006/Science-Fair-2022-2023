eb820e0fc0a60856aa0b619af538f1d1af6fbe2f
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2023-01-02 15:25:20 -0500

    Update sounddist.py

--- odas/sounddist.py
+++ odas/sounddist.py
@@ -7,21 +7,20 @@ x = np.array([load_audio_file("mic1.wav"),
               load_audio_file("mic3.wav"),
               load_audio_file("mic4.wav")])
 
-# Set the sampling frequency (in Hz)
-fs = 44100
+def closest_microphone(x):
+	# Set the sampling frequency (in Hz)
+	fs = 44100
 
-# Set the distance between microphones (in meters)
-d = 0.1
+	# Set the distance between microphones (in meters)
+	d = 0.035
 
-# Estimate the distance of the sound source from each microphone using the GCC algorithm
-z = odas.gcc(x, fs)
-distance_mic1 = z[0] * fs * 0.5 * d
-distance_mic2 = z[1] * fs * 0.5 * d
-distance_mic3 = z[2] * fs * 0.5 * d
-distance_mic4 = z[3] * fs * 0.5 * d
+	# Estimate the distance of the sound source from each microphone using the GCC algorithm
+	z = odas.gcc(x, fs)
+	distance_mic1 = z[0] * fs * 0.5 * d
+	distance_mic2 = z[1] * fs * 0.5 * d
+	distance_mic3 = z[2] * fs * 0.5 * d
+	distance_mic4 = z[3] * fs * 0.5 * d
+	return min(distance_mic1, distance_mic2, distance_mic3, distance_mic4)
 
-# Print the estimated distance of the sound source from each microphone
-print("Distance from microphone 1:", distance_mic1, "meters")
-print("Distance from microphone 2:", distance_mic2, "meters")
-print("Distance from microphone 3:", distance_mic3, "meters")
-print("Distance from microphone 4:", distance_mic4, "meters")
+print(closest_microphone(x))
+
