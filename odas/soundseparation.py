d5acf7696d64212a27b69f86f043acca4cd44733
Jesse Choe <67716130+jessechoe10@users.noreply.github.com>
2023-01-03 11:01:57 -0500

    Update soundseparation.py

--- odas/soundseparation.py
+++ odas/soundseparation.py
@@ -1,11 +1,7 @@
 import odas
 import scipy.io.wavefile
 
-audio_data, fs = odas.read()
-
-sources = odas.doa(audio_data, fs)
-
-loudest_sources = sorted(sources, key=lambda x: x.energy, reverse=True)[:4]
-
-for i, source in enumerate(loudest_sources):
-	scipy.io.wavefile.write('source{}.wav'.format(i+1), fs, source.signal)
+def sound_separation(audio_data, fs):
+	sources = odas.doa(audio_data, fs)
+	loudest_sources = sorted(sources, key=lambda x: x.energy, reverse=True)[:4]
+	return loudest_sources
